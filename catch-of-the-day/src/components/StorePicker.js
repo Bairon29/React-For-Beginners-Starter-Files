e7ff6e3b97e1d18ea66a7b8be46ad4d347c14b31
Bairon29 <baironvasquez29@gmail.com>
2016-12-27 13:10:24 -0500

    13 videos down, 17 to go. react is awesome so far. I feel that i'm going back end sometimes when I really not.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,14 +1,30 @@
 import React from 'react';
-
+import { getFunName } from '../helpers';
 class StorePicker extends React.Component {
+
+// constructor(){
+//   super();
+//   this.goToStore = this.goToStore.bind(this);
+// }
+
+  goToStore(event){
+    event.preventDefault();
+console.log(this.storeInput.value);
+const storeId = this.storeInput.value;
+this.context.router.transitionTo(`/store/${storeId}`);
+  }
   render(){
     return (
-      <form classname="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
           <h2>Please Enter a Store </h2>
-          <input typre="text" require placeholder="store name" />
+          <input type="text" required placeholder="Store Name"  defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
           <button type="submit"> Visit Store - </button>
       </form>
       )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 export default StorePicker;
